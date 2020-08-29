360a3916e99421076082f1a839f25b901ea6a158
riri <ririho0112@gmail.com>
2020-08-29 20:48:31 +0900

    Update managementpage1.php

--- managementpage1.php
+++ managementpage1.php
@@ -6,6 +6,16 @@
         <title>管理者ページ</title>
     </head>
     <body>
-        
+        <!-- user_idを指定するとその人のカレンダーを閲覧することができる -->
+        <form>
+            <input type="number" name="userID">
+            <input type="submit" name="user_reference" value="閲覧">
+        </form>
+        <?php
+        if(isset($_POST["userID"])){
+            $id=$_POST["userID"];
+            //このidのテーブルを参照・カレンダーを表示できる
+        }
+        ?>
     </body>
 </html>
