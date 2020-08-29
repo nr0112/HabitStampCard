4b27af239e644a68045e5075456129c05f83c4b8
riri <ririho0112@gmail.com>
2020-08-29 20:48:45 +0900

    Update rankingpage.php

--- rankingpage.php
+++ rankingpage.php
@@ -1,1 +1,11 @@
 <!-- ランキング掲示板ページ -->
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>ランキング掲示板</title>
+    </head>
+    <body>
+
+    </body>
+</html>
