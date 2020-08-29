10d8c0eacd827abea7490a61e51457ef9336e9a1
riri <ririho0112@gmail.com>
2020-08-29 20:30:45 +0900

    Update resisterpage.php

--- resisterpage.php
+++ resisterpage.php
@@ -8,22 +8,25 @@
     </head>
     <body>
 
+
     <form action="" method="POST">
         <!--本登録用入力フォーム--> 
         <fieldset>
             <legend>基本情報</legend>
                 <label>登録用メールアドレス
-                    <input type="text" name="new_mail" placeholder="メールアドレス"><br>
+                    <input type="text" name="new_mail" placeholder="メールアドレス">
                 </label>
                 <label>ユーザー名
-                    <input type="text" name="username" placeholder="ユーザー名"><br>
+                    <input type="text" name="username" placeholder="ユーザー名">
                 </label>
-                <label>パスワード</label>
-                    <p><input type="text" name="password" placeholder="パスワード"></p>
-                    <p>確認用パスワード</p>
-                    <p><input type="text" name="password2" placeholder="パスワード"></p>
+                <label>パスワード
+                    <input type="text" name="password" placeholder="パスワード">
+                </label>
+                <label>確認用パスワード
+                    <input type="text" name="password2" placeholder="パスワード">
+                </label>
                 <label>目標起床時間
-                    <input type="text" name="wakeup" placeholder="起床時間">
+                    <input type="time" name="wakeup" placeholder="起床時間">
                 </label>
         </fieldset>
 
@@ -32,12 +35,23 @@
     </form>
 
     <?php
+    //---------------テーブル1------------------------------------------
+    $sql = "CREATE TABLE IF NOT EXISTS db_users"
+    ."("
+    ."id INT AUTO_INCREMENT PRIMARY KEY,"
+    ."mail TEXT,"
+    ."username varchar(20),"
+    ."password TEXT,"
+    ."wakeup TEXT,"
+    ."date_resister TEXT"
+    .");";
+    $stmt = $pdo -> query($sql);
+    //---------------------------------------------------------------------
 
     require_once("pdo.php");
     $pdo = pdo_connect();
 
     //登録情報をデータベースに登録する
-
     if (isset($_POST["new_mail"])&&isset($_POST["username"])&&isset($_POST["password"])){
         if ($_POST["password"]==$_POST["password2"]){
             //セッションの開始
@@ -46,6 +60,7 @@
             $mail= $_POST["new_mail"];
             $username = $_POST["username"];
             $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+            $wakeup = $_POST["wakeup"];
             $date_resister = date("Y/m/d");
 
             //確認用に、値をセッション変数に格納
@@ -53,6 +68,7 @@
             $_SESSION["username_resister"] = $username;
             //ハッシュ化されたものが表示されるのを防ぐため、POSTから受け取ったそのままで格納
             $_SESSION["password_resister"] = $_POST["password"];
+            $_SESSION["wakeup"] = $wakeup;
             $_SESSION["date_resister"] = $date_resister;
             
             try{
@@ -61,6 +77,7 @@
                 $stmt_insert -> bindParam(':mail', $mail, PDO::PARAM_STR);
                 $stmt_insert -> bindParam(':username', $username, PDO::PARAM_STR);
                 $stmt_insert -> bindParam(':password', $password, PDO::PARAM_STR);
+                $stmt_insert -> bindParam(':wakeup', $wakeup, PDO::PARAM_STR);
                 $stmt_insert -> bindParam(':date_resister', $date_resister, PDO::PARAM_STR);
                 $stmt_insert -> execute();
 
@@ -72,6 +89,7 @@
                       登録メールアドレス：'. $_SESSION["mail_resister"].'<br>
                       ユーザー名：'.$_SESSION["username_resister"].'<br>
                       パスワード：'. mb_substr($_SESSION["password_resister"],0,2,"UTF-8").str_repeat("*",mb_strlen($_SESSION["password_resister"],"UTF-8")-2).'<br>
+                      目標起床時間:'.$_SESSION["wakeup"].'<br>;
                       登録日：'.$_SESSION["date_resister"].'<br>';
                 //登録完了後71行目のユーザーIDをもとにテーブルを作成
                 $sql_create_table="CREATE TABLE $id"
@@ -106,18 +124,20 @@
             echo 'パスワードが一致しません。もう一度入力してください！';
         }
         
-    }elseif (isset($_POST["new_mail"])==FALSE or $_POST["new_mail"]==""){
+    }elseif(isset($_POST["new_mail"])==FALSE or $_POST["new_mail"]==""){
         echo 'メールアドレスが未入力です。';
 
-    }elseif (isset($_POST["username"])==FALSE or $_POST["username"]==""){
+    }elseif(isset($_POST["username"])==FALSE or $_POST["username"]==""){
         echo 'ユーザー名が未入力です。';
 
-    }elseif (isset($_POST["password"])==FALSE or $_POST["password"]==""){
+    }elseif(isset($_POST["password"])==FALSE or $_POST["password"]==""){
         echo 'パスワードが未入力です。';
 
-    }elseif (isset($_POST["password2"])==FALSE or $_POST["password2"]==""){
+    }elseif(isset($_POST["password2"])==FALSE or $_POST["password2"]==""){
         echo '確認用パスワードが未入力です。';
 
+    }elseif(isset($_POST["wakeup"])==FALSE){
+        echo '目標起床時間が未入力です。';
     }
 
     
