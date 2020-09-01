0b9e9b0b4acd28b0c1f4e0df64327dca7863d3b0
classjoke <67227264+classjoke@users.noreply.github.com>
2020-09-01 23:35:21 +0900

    Update rankingpage.php

--- rankingpage.php
+++ rankingpage.php
@@ -1,9 +1,20 @@
 <!-- ランキング掲示板ページ -->
+
+<?php
+    require_once("pdo.php");
+    $pdo = pdo_connect();
+
+    $select = "SELECT * FROM db_users";
+    $stmt = $pdo -> query($select);
+    $results = $stmt->fetchAll();
+    var_dump($results);
+?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
         <title>ランキング掲示板</title>
+
     </head>
     <body>
     </body>
