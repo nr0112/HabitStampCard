1a4afdc53555e8d0ebd6f21b0c619976125d1d1c
riri <ririho0112@gmail.com>
2020-09-01 22:01:55 +0900

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -222,8 +222,7 @@
 </head>
 <body>
 
-<?php var_dump($ym); var_dump($timestamp); ?>
-        <div class="container">
+    <div class="container">
     <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
         <table class="table table-bordered">
             <tr>
