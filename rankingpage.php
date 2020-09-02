8ae9e8ed8bff12ed1f996c343938eadd737d7300
riri <ririho0112@gmail.com>
2020-09-02 22:37:31 +0900

    Update rankingpage.php

--- rankingpage.php
+++ rankingpage.php
@@ -37,6 +37,8 @@
 
     </head>
     <body>
-    <a href="mypage1.php"></a>
+    <a href="loginpage.php">ログインページ</a>
+    <a href="mypage1.php">マイページ</a>
+    <a href="logoutpage.php">ログアウト</a>
     </body>
 </html>
