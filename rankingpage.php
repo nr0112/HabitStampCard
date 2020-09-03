90ed5ed01ecd99dc92bd2514ed29da66e3d470bc
riri <ririho0112@gmail.com>
2020-09-03 21:42:13 +0900

    Update rankingpage.php

--- rankingpage.php
+++ rankingpage.php
@@ -6,9 +6,39 @@
     <head>
         <meta charset="UTF-8">
         <title>ランキング掲示板</title>
-        <h1>きょうはやくおきたひとランキング</h1>
+        <style>
+            a{
+                text-decoration: none;
+                float: right;
+            }
+            body{
+                background-color: peachpuff;
+            }
+            .rank-set{
+                display: flex;
+            }
+            .time{
+                width: 800px;
+                margin-left: 50px;
+            }
+            .rank1{
+                color: red;
+                font-weight: bold;
+            }
+            .rank2{
+                font-weight: bold;
+            }
+            .box{
+                background-color: ivory;
+                padding: 10px 5px;
+            }
+            h1{
+                color: red;
+            }
+        </style>
     </head>
-    <body>
+    <h1>きょうはやくおきたひとランキング</h1>
+    <hr>
         <?php
             require_once("pdo.php");
             $pdo = pdo_connect();
@@ -33,12 +63,17 @@
             foreach($name_time as $key => $row){
                 $number++;
                 $time = date('H:i', strtotime($row));
-                echo $number."位:　";
-                echo $key."さん: ".$time."おきました。";
-                echo "<br>";
+                echo "<div class='box'>";
+                echo "<div class=rank-set>";
+                echo "<div class='rank1'> $number 位　</div>";
+                echo "<div class='rank2'> $key さん </div>";
+                echo "</div>";
+                echo "<div class='time'>  $time におきました！ </div>";
+                echo "</div>";
+                echo "<hr>";
             }
         ?>
-    <a href="mypage1.php">マイページへ</a>
     <a href="logoutpage.php">ログアウト</a>
+    <a href="mypage1.php">マイページ</a>
     </body>
 </html>
